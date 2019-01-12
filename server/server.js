3f6bcd3a10484d0ea51410489d1e049b900f3915
JeanCapelle <capelle.jean@gmail.com>
2019-01-12 17:55:35 +0100

    add server part

---
+++ server/server.js
@@ -0,0 +1,16 @@
+
+var io = require('socket.io')(1337);
+
+console.log('Socket listen on port 1337 : ');
+
+io.on('connection',  socket => {
+  console.log('Client connected');
+
+  socket.on('message', msg => {
+    console.log(`New message : ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
